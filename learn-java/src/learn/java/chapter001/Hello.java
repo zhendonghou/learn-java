1d52653586420114626d491b9d8d9f0dd6f7fb1a
hou zhendong <93426718+zhendonghou@users.noreply.github.com>
2025-03-08 18:11:30 +0800

    Add files via upload
    just first upload

---
+++ learn-java/src/learn/java/chapter001/Hello.java
@@ -0,0 +1,10 @@
+package learn.java.chapter001;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World! "  + i);
+        }
+    }
+}
